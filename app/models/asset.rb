f715854e97f6f4b54be89cbe273912e943f1c10d
Yan 'YncYrYdYbYl' Minagawa <t@c-base.org>
2011-01-06 00:46:12 +0100

    refact

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,6 @@
 class Asset < ActiveRecord::Base
   belongs_to :submission
+  attr_accessible :screenshot_file_name
   has_attached_file :screenshot, :styles => {
     :thumb => "150x150#",
     :medium => "300x300",
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,7 +1,5 @@
 class Submission < ActiveRecord::Base
-  attr_accessible :title, :user_id, :category_id, :plattform_id, :url, :description
+  attr_accessible :title, :user_id, :category_id, :plattform_id, :url, :description, :assets_attributes
   has_many :assets
   accepts_nested_attributes_for :assets
-  has_attached_file :screenshot, :path => "/tmp/iii"
-
 end
