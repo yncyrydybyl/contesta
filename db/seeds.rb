86b63afc7392214448bd867e274ed78bf03786f2
Anselm Helbig <mnemonikk@c-base.org>
2011-01-07 19:03:56 +0100

    pimping seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,10 @@ end
 
 #["meego", "ios", "android"].each do |plattform|
 #  Plattform.find_or_create_by_shortname
+
+open("db/pages.txt") do |pages|
+  pages.read.each_line do |page|
+    name, permalink, content = page.chomp.split("|")
+    Page.create!(:name => name, :permalink => permalink, :content => content)
+  end
+end
