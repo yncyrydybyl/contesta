bf2670fb7a28b71fe4ee2b676ef7b942cae99947
Yan 'YncYrYdYbYl' Minagawa <t@c-base.org>
2011-01-07 22:44:22 +0100

    fixed model dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-
+  has_many :submissions
   def is_admin?
     email == "t@c-base.org"
   end
