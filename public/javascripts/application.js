4dd7d60a3788e31b0ddc520b2b0b46deb4ba0a69
Yan 'YncYrYdYbYl' Minagawa <t@c-base.org>
2011-01-18 20:35:16 +0100

    added a javascript lighbox pluging to the screenshots in the submission listing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,4 +5,15 @@ $(document).ready(function(){
   $('.clickable').click(function() {
     window.location = $(this).attr("url");
   });
+  $('.lightbox').lightBox({
+  overlayBgColor: '#FFF',
+  overlayOpacity: 0.6,
+  imageLoading: '/images/lightbox-ico-loading.gif',
+  imageBtnClose: '/images/lightbox-btn-close.gif',
+  imageBtnPrev: '/images/lightbox-btn-prev.gif',
+  imageBtnNext: '/images/lightbox-btn-next.gif',
+  containerResizeSpeed: 350,
+  txtImage: 'Icmagem',
+  txtOf: 'de'
+   });        
 });
