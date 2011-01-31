67b1f201db3d28976c3465ad39ae096dca07f01b
Yan 'YncYrYdYbYl' Minagawa <t@c-base.org>
2011-01-31 15:26:52 +0100

    changed lightbox plugin to only show screenshots in submission

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,15 +5,21 @@ $(document).ready(function(){
   $('.clickable').click(function() {
     window.location = $(this).attr("url");
   });
-  $('.lightbox').lightBox({
-  overlayBgColor: '#FFF',
-  overlayOpacity: 0.6,
-  imageLoading: '/images/lightbox-ico-loading.gif',
-  imageBtnClose: '/images/lightbox-btn-close.gif',
-  imageBtnPrev: '/images/lightbox-btn-prev.gif',
-  imageBtnNext: '/images/lightbox-btn-next.gif',
-  containerResizeSpeed: 350,
-  txtImage: 'Icmagem',
-  txtOf: 'de'
-   });        
+
+  var boxen = [];
+
+  $('a[rel*=lightbox_]').each(function() {
+    if ($.inArray($(this).attr('rel'),boxen)) boxen.push($(this).attr('rel'));
+  });
+  $(boxen).each(function(i,val) { 
+     $('a[rel='+val+']').lightBox({
+      overlayBgColor: '#FFF',
+      overlayOpacity: 0.6,
+      imageLoading: '/images/lightbox-ico-loading.gif',
+      imageBtnClose: '/images/lightbox-btn-close.gif',
+      imageBtnPrev: '/images/lightbox-btn-prev.gif',
+      imageBtnNext: '/images/lightbox-btn-next.gif',
+      containerResizeSpeed: 350
+     });
+  });
 });
