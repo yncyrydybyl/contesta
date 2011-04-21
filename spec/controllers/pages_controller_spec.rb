4edec80d4e54a24da2c9412bf6476464aec997eb
Yan 'YncYrYdYbYl' Minagawa <t@c-base.org>
2011-04-21 13:47:32 +0200

    trying mocha

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,7 +6,7 @@ describe PagesController do
   describe 'with unauthorized users' do
 
     before :each do
-      @page = stub_model(Page)
+      #Page.any_@page = stub_model(Page)
     end
 
     after :each do
@@ -30,7 +30,12 @@ describe PagesController do
   end
 
   describe 'logged in admin user' do
+    before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
     before :each do
+      DatabaseCleaner.start
       @admin = Factory.create(:admin)
       sign_in @admin
     end
@@ -41,20 +46,15 @@ describe PagesController do
 
     describe 'new' do
       it 'should allow bulding pages' do
-        Page.should_receive(:new).at_least(:once)
+        #Page = mock
+        Page.expects(:new).at_least_once
         get :new
         response.should be_ok
       end
 
       it 'should assign the page instance variable' do
-        Page.stub(:new => 'new_page')
-        get :new
-
-        assigns[:page].should == 'new_page'
-      end
-
-      it 'should assign serialize pages xml on request' do
-        Page.stub(:new => [])
+        #Page = mock
+        Page.stubs(:new => [])
         get :new, :format => :xml
 
         response.should be_ok
@@ -68,7 +68,7 @@ describe PagesController do
         @page = Factory.build(:page)
         post :create, :page => @page
         response.should be_redirect 
-        response.should redirect_to :action => :show, :id => 1 
+        response.should redirect_to :action => :show, :id => Page.last.id 
       end
     end
     describe 'show' do
@@ -76,12 +76,6 @@ describe PagesController do
         @page = Factory.create(:page, :permalink => "slug")
         get :show, :permalink => "slug"
         response.should be_ok
-      end
-      it 'should render markdown to html' do
-        @page = Factory.create(:page, :content => "* eins\n* zwei")
-        get :show, :id => @page.id
-        response.should be_ok
-        response.body.should include_text("djska")
       end
     end
   end
