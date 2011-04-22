0ecdbfabafcafda5480f32292b6cdacf6a5714b8
Yan 'YncYrYdYbYl' Minagawa <t@c-base.org>
2011-04-22 22:43:52 +0200

    added users page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,16 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
   has_many :submissions
+  
+  named_scope :with_at_least_one_submission, :joins => :submissions, :group => 'users.id'
+  
+  def display_name
+    if first_name and last_name
+      return first_name + " "+ last_name
+    else
+      return email.split('@')[0]+"@***."+email.split('.').last
+    end
+  end
   def admin?
     ["t@c-base.org","zehjotkah@googlemail.com", "jukey@ju-key.de"].include? email
   end
